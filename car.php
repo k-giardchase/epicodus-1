389fb29c05625d244fe5ba77d1e90a83dedbe387
Kyle Giard-Chase <kgiardch@gmail.com>
2015-03-02 12:22:42 -0800

    Add get and set methods

--- car.php
+++ car.php
@@ -1,15 +1,17 @@
 <?php
     class Car
     {
-        public $make_model;
-        public $price;
-        public $miles;
+        private $make_model;
+        private $price;
+        private $miles;
+        private $image_path;
 
 
-        function __construct($make_model, $price, $miles) {
+        function __construct($make_model, $price, $miles, $image_path) {
             $this->make_model = $make_model;
             $this->price = $price;
             $this->miles = $miles;
+            $this->image_path = $image_path;
 
         }
 
@@ -19,14 +21,62 @@
 
         }
 
+        function setMakeModel()
+        {
+             $this->make_model;
+        }
+
+        function setPrice($new_price)
+        {
+            $float_price = (float) $new_price;
+            if ($float_price != 0) {
+                $formatted_price = number_format($float_price, 2);
+                $this->price = $formatted_price;
+            }
+        }
+
+        function setMiles()
+        {
+             $this->miles;
+        }
+
+        function setImage() {
+
+            $this->image_path;
+        }
+
+        function getMakeModel ()
+        {
+            return $this->make_model;
+        }
+
+        function getPrice()
+        {
+            return $this->price;
+
+        }
+
+        function getMiles ()
+        {
+            return $this->miles;
+        }
+
+        function getImage () {
+
+            return $this->image_path;
+
+        }
 
 
     }
 
-    $porsche = new Car("2014 Porsche 911", 114989, 798989);
-    $ford = new Car("2011 Ford F450", 12445, 98989);
-    $lexus = new Car("2013 Lexus 350", 55584, 8888);
-    $mercedes = new Car("Mercedes Benz CLS550", 390000, 37979);
+    $porsche = new Car("2014 Porsche 911", 114989, 798989, "<img src='porshe.jpg'>");
+    $ford = new Car("2011 Ford F450", 12445, 98989, "<img src='ford.jpg'>");
+    $lexus = new Car("2013 Lexus 350", 55584, 8888, "<img src='lexus.jpg'>");
+    $mercedes = new Car("Mercedes Benz CLS550", 390000, 37979, "<img src='mercedes.jpg'>");
+    $mercedes->setPrice(9999999);
+    $mercedes->setMiles(2);
+    $mercedes->setMakeModel($lexus);
 
     $cars = array($porsche, $ford, $lexus, $mercedes);
 
@@ -36,6 +86,8 @@
         array_push($cars_matching_search, $car);
     }
 }
+
+
 
 ?>
 
@@ -49,10 +101,11 @@
     <ul>
         <?php
             foreach ($cars_matching_search as $car) {
-                echo "<li> $car->make_model </li>";
+                echo "<li>" . $car->getMakeModel() . "</li>";
                 echo "<ul>";
-                    echo "<li> $$car->price </li>";
-                    echo "<li> Miles: $car->miles </li>";
+                    echo "<li> $" . $car->getPrice() . "</li>";
+                    echo "<li> Miles:" .  $car->getMiles() . "</li>";
+                    echo "<li>" . $car->getImage() . "</li>";
                 echo "</ul>";
             }
         ?>
